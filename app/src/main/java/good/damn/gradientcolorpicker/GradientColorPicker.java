498b3acb6041f48c05e2a5f4c0d0f5b0240dc234
GoodDamn <76108467+GoodDamn@users.noreply.github.com>
2023-07-01 06:19:57 +0300

    Getting color information from touch

--- app/src/main/java/good/damn/gradientcolorpicker/GradientColorPicker.java
+++ app/src/main/java/good/damn/gradientcolorpicker/GradientColorPicker.java
@@ -1,5 +1,6 @@
 package good.damn.gradientcolorpicker;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -7,6 +8,7 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -14,7 +16,7 @@ import androidx.annotation.Nullable;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class GradientColorPicker extends View {
+public class GradientColorPicker extends View implements View.OnTouchListener{
 
     private static final String TAG = "GradientColorPicker";
 
@@ -46,15 +48,34 @@ public class GradientColorPicker extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawBitmap(mBitmapGradient,0,0,mPaint);
+        canvas.drawRect(0,0,50,50,mPaint);
     }
 
+    @SuppressLint("DrawAllocation")
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
 
+        setOnTouchListener(null);
         Log.d(TAG, "onLayout: WIDTH_GRAD: " + mBitmapGradient.getWidth() + " WIDTH: " + getWidth());
         if (mBitmapGradient.getWidth() < getWidth()) {
             mBitmapGradient = Bitmap.createScaledBitmap(mBitmapGradient, getWidth(), getHeight(), false);
         }
+        setOnTouchListener(this);
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+
+        int x = (int) motionEvent.getX();
+        int y = (int) motionEvent.getY();
+
+        if (y < 0 || y >= mBitmapGradient.getHeight())
+            return true;
+
+        mPaint.setColor(mBitmapGradient.getPixel(x, y));
+
+        invalidate();
+        return true;
     }
 }
